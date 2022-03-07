0d367d761fe996b88a03e799381ec11b55dd588e
Newbie0202 <88521699+Newbie0202@users.noreply.github.com>
2022-03-07 17:00:47 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const nameGirl = 'Bạn bánh bao ơi!';
 const giftUrl = 'http://nodemy.vn';
-const eventName = 'Chúc Mừng 8-3 ';
+const eventName = 'Chúc Mừng 8-3';
 const titleCard = 'Tặng bạn hay lý do lý trấu :V';
 const contentCard = 'Mừng ngày phụ nữ , mừng ngày bạn gì xinh xinh :v (nhưng mà hơi ngố :V)🥰😘😘. Còn nhiều điều chờ phía trước , mạnh mẽ lên bạn gì đó ơi :V';
 
@@ -8,6 +8,6 @@ const contentCard = 'Mừng ngày phụ nữ , mừng ngày bạn gì xinh xinh 
 // Cần hỗ trợ hãy liên hệ: 
 // Mr-Nam http://facebook.com/nam.nodemy
 // Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
-const giftImage = 'hotgirl.png';
+const giftImage = 'hot-girl.png';
 const base64 = '';
 const giftImageBase64 = "data:image/png;base64, " + base64;
